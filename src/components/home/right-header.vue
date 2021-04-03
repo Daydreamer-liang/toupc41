7b13d3768b8e911573bb639dbcf08a592b38aff8
zhuxiangliang <916447016@qq.com>
2021-04-03 10:48:04 +0800

    主页-个人信息退出，删除token完成

--- src/components/home/right-header.vue
+++ src/components/home/right-header.vue
@@ -9,12 +9,13 @@
         <img
           src="https://img2.baidu.com/it/u=2890165884,1588619991&fm=26&fmt=auto&gp=0.jpg"
         />
-        <el-dropdown trigger='click'>
+        <!-- 下拉菜单 -->
+        <el-dropdown @command="clickmand" trigger="click">
           <span class="el-dropdown-link"> 下拉菜单 </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>个人信息</el-dropdown-item>
-            <el-dropdown-item>git地址</el-dropdown-item>
-            <el-dropdown-item>退出</el-dropdown-item>
+            <el-dropdown-item command="info">个人信息 </el-dropdown-item>
+            <el-dropdown-item command="git">git地址</el-dropdown-item>
+            <el-dropdown-item command="out">退出</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-row>
@@ -23,7 +24,40 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      userInfo: {} // 用户信息
+    }
+  },
+  methods: {
+    clickmand (command) {
+      // 退出登录
+      if (command === 'info') {
+        location.href('个人信息地址')
+      } else if (command === 'git') {
+        window.location.href('git地址')
+      } else {
+        //   退出系统  1.删除token 跳转登录页
+        localStorage.removeItem('user-token')
+        // push从哪来回哪去
+        this.$router.push('/login')
+      }
+    }
+  },
+  created () {
+    //   获取用户信息
+    const token = localStorage.getItem('user-token')
+    this.$axios({
+      url: '/user/profile',
+      Headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then((result) => {
+      this.userInfo = result.data.data
+    })
+  }
+}
 </script>
 
 <style lang="less" scoped>

d42f6aeee28a4268216629f162b2358282e5fe74
zhuxiangliang <916447016@qq.com>
2021-04-04 08:26:30 +0800

    主页-评论列表-页面结构

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -3,14 +3,47 @@
   <el-card>
     <!-- 使用 components 文件下 common的面包屑导航 -->
 
-    <Bread-Crumb>
+    <Bread-Crumb slot="header">
       <template slot="title"> 评论管理 </template>
     </Bread-Crumb>
+    <!-- 表格组件 -->
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <style>

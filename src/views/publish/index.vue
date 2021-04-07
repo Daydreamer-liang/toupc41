f2c73e332ea057ed1f493e30831cd09f68dbcd47
zhuxiangliang <916447016@qq.com>
2021-04-07 14:58:04 +0800

    发表文章 -结构

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -4,11 +4,41 @@
     <Bread-Crumb slot="header">
       <template slot="title">发布文章</template>
     </Bread-Crumb>
+    <el-form label-width="80px">
+      <el-form-item label="标题">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item label="内容">
+        <el-input type="textarea" :rows="4" placeholder="请输入内容">
+        </el-input>
+      </el-form-item>
+      <el-form-item label="封面">
+        <el-radio-group v-model="radio">
+          <el-radio :label="1">备选项</el-radio>
+          <el-radio :label="2">备选项</el-radio>
+          <el-radio :label="3">备选项</el-radio>
+          <el-radio :label="4">备选项</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道">
+        <el-input></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发表</el-button>
+        <el-button>存入草稿</el-button>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      radio: 10
+    }
+  }
+}
 </script>
 
 <style>
